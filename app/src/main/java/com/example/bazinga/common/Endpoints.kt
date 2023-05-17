a366e544b2b7c1132f0583bd50813c1f902ef385
michalnx13 <michalnx13@gmail.com>
2023-05-17 13:56:21 +0200

    initial di

---
+++ app/src/main/java/com/example/bazinga/common/Endpoints.kt
@@ -0,0 +1,6 @@
+package com.example.bazinga.common
+
+object Endpoints {
+
+    const val BASE_URL = "https://api.spaceflightnewsapi.net"
+}
---
+++ app/src/main/java/com/example/bazinga/di/AppModule.kt
@@ -0,0 +1,35 @@
+package com.example.bazinga.di
+
+import com.example.bazinga.common.Endpoints
+import com.example.bazinga.data.remote.NewsApi
+import com.example.bazinga.data.repository.NewsRepositoryImpl
+import com.example.bazinga.domain.repository.NewsRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideNewsApi(): NewsApi {
+        return Retrofit
+            .Builder()
+            .baseUrl(Endpoints.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(NewsApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNewsRepository(newsApi: NewsApi): NewsRepository {
+        return NewsRepositoryImpl(newsApi)
+    }
+}
