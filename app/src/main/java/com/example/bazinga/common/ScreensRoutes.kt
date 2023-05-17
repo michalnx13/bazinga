a90f825580f6a1e16cbc6df08ba7e9c4efbff12b
michalnx13 <michalnx13@gmail.com>
2023-05-17 18:08:16 +0200

    news list view

---
+++ app/src/main/java/com/example/bazinga/common/ScreensRoutes.kt
@@ -0,0 +1,6 @@
+package com.example.bazinga.common
+
+object ScreensRoutes {
+    const val NEWS_LIST_SCREEN = "news_list_screen"
+    const val NEWS_DETAILS_SCREEN = "news_details_screen"
+}
---
+++ app/src/main/java/com/example/bazinga/presentation/newsList/components/NewsListScreen.kt
@@ -0,0 +1,54 @@
+package com.example.bazinga.presentation.newsList.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.example.bazinga.presentation.Screen
+import com.example.bazinga.presentation.newsList.NewsListViewModel
+
+@Composable
+fun NewsListScreen(
+    navController: NavController,
+    viewModel: NewsListViewModel = hiltViewModel()
+) {
+    val state = viewModel.state.value
+    Box(modifier = Modifier.fillMaxSize()) {
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            items(state.news) { news ->
+                NewsListItem(
+                    news = news,
+                    onItemClickAction = {
+                        navController.navigate(Screen.NewsDetailsScreen.route + "${it.id}")
+                    }
+                )
+            }
+        }
+        if (state.error != null) {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(12.dp)
+                    .align(Alignment.Center),
+                text = state.error,
+                color = MaterialTheme.colorScheme.error,
+                textAlign = TextAlign.Center
+            )
+        }
+        if (state.isLoading) {
+            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+        }
+    }
+}
