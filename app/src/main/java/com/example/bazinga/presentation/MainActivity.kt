487cc581c2ca3d616e5bbb1eb4172ceb9949e80f
michalnx13 <michalnx13@gmail.com>
2023-05-17 18:09:06 +0200

    added navigation, MainActivity as entry point

--- app/src/main/java/com/example/bazinga/presentation/MainActivity.kt
+++ app/src/main/java/com/example/bazinga/presentation/MainActivity.kt
@@ -6,12 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.bazinga.presentation.newsDetails.NewsDetailsScreen
+import com.example.bazinga.presentation.newsList.components.NewsListScreen
 import com.example.bazinga.presentation.ui.theme.BazingaTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,25 +26,24 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.NewsListScreen.route
+                    ) {
+                        composable(
+                            route = Screen.NewsListScreen.route
+                        ) {
+                            NewsListScreen(navController = navController)
+                        }
+                        composable(
+                            route = Screen.NewsDetailsScreen.route + "/{newsId}"
+                        ) {
+                            NewsDetailsScreen()
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BazingaTheme {
-        Greeting("Android")
     }
 }
