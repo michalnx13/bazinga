01d33f246396ce6a06dc1504a44e4f481a08aa1b
michalnx13 <michalnx13@gmail.com>
2023-05-18 18:32:10 +0200

    added okhttp client

--- app/src/main/java/com/example/bazinga/di/AppModule.kt
+++ app/src/main/java/com/example/bazinga/di/AppModule.kt
@@ -10,6 +10,8 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -19,12 +21,20 @@ import javax.inject.Singleton
 object AppModule {
 
     @Provides
+    fun provideOkHttpClient(): OkHttpClient {
+        val okHttpBuilder = OkHttpClient.Builder()
+        okHttpBuilder.addInterceptor(HttpLoggingInterceptor())
+        return okHttpBuilder.build()
+    }
+
+    @Provides
     @Singleton
-    fun provideNewsApi(): NewsApi {
+    fun provideNewsApi(okHttpClient: OkHttpClient): NewsApi {
         return Retrofit
             .Builder()
             .baseUrl(Endpoints.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
             .build()
             .create(NewsApi::class.java)
     }
